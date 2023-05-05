2922b0892ee8bcdad01bfc36dfc59848eb37b906
vaibhav-chandraker <Chandrakervaibhav2830@gmail.com>
2023-05-05 16:32:08 +0530

    add variables.php

---
+++ 1_variables.php
@@ -0,0 +1,18 @@
+<?php
+
+// variables in php are case sensitive
+$name = "vaibhav";
+$salary = 200;
+echo "His name is $name and is salary is $salary <br>";
+
+// keywords in php are not case sensitive
+ECHO "His name is $name and is salary is $salary"
+
+?>
+
+<!-- Rules for writing a variables:
+    1. variables in php are case sensitive
+    2. variables are witten with a prefix of $
+    3. variables can start with any letter or underscore
+    4. it cannot start with a number
+    5. it can only contain letters numbers and underscore (no special characters are allowed) -->
