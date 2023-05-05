6f16771e9be888b381d7323472d59fa5bc0c4d55
vaibhav-chandraker <Chandrakervaibhav2830@gmail.com>
2023-05-05 17:10:11 +0530

    data_types.php

---
+++ 2_data_types.php
@@ -0,0 +1,42 @@
+<?php
+
+// strings
+$name = "vaibhav";
+$hello = 'vaibhav';
+echo var_dump($name);
+echo "<br>";
+
+//interger
+$number = 200;
+$NUMBER = -200;
+echo var_dump($number);
+echo "<br>";
+
+//FLOAT
+$NUMBER = 200.0;
+$number = -200.0;
+echo var_dump($number);
+echo "<br>";
+
+// boolean
+$B1 = true;
+$B2 = false;
+echo var_dump($B1);
+echo "<br>";
+
+// Arrays
+$friends = array("vaibhav","harsh","saket","sourabh");
+echo var_dump($friends);
+echo "<br>";
+echo var_dump($friends[0]);
+echo "<br>";
+echo var_dump($friends[1]);
+echo "<br>";
+echo var_dump($friends[2]);
+echo "<br>";
+
+// null
+$value = null;
+echo var_dump($value);
+
+?>
