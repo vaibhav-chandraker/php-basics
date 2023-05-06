7b214d7a71ea77aa65e9a6d50b641398edb68c36
vaibhav-chandraker <Chandrakervaibhav2830@gmail.com>
2023-05-06 23:19:44 +0530

    control_statements.php

---
+++ 5_if_else.php
@@ -0,0 +1,15 @@
+<?php
+
+$age = 25;
+
+if ($age>18){
+    echo "you can drive bikes and cars<br>";
+}
+elseif($age>12){
+    echo "you can drive bicycle<br>";
+}
+else{
+    echo "you cannot drive at all";
+}
+
+?>
