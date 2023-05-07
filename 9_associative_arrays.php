d5202f1ffa6840322b1cc95d7aad87fd619d02dd
vaibhav-chandraker <Chandrakervaibhav2830@gmail.com>
2023-05-07 13:19:52 +0530

    associative_arrays.php

---
+++ 9_associative_arrays.php
@@ -0,0 +1,21 @@
+<?php
+
+// array 
+$arr = array("green","blue","red"); //these are numeric arrays because we can do indexing with numbers
+echo $arr[1];
+echo "<br>";
+
+$favcol = array("vaibhav"=>"orange","saket"=>"blue",25=>"red");
+echo $favcol["vaibhav"];
+echo "<br>";
+echo $favcol["saket"];
+echo "<br>";
+echo $favcol[25];
+echo "<br>";
+
+foreach($favcol as $key => $value){
+    echo "the value of $key is $value<br>";
+}
+
+
+?>
