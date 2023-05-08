af57d53077cc44e3524434bf8a03c2d0c58d6b60
vaibhav-chandraker <Chandrakervaibhav2830@gmail.com>
2023-05-08 13:08:16 +0530

    get_post.php
    used the get and post method to submit  values of the form to file no 13 ie. form.php

---
+++ 12_get_post.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>please fill the below form:</h1>
+    <form action="/13_form.php" method = "post">
+
+    E-MAIL - <input type="text" name="e-mail"><br>
+    Password - <input type="text" name="pass"><br>
+    <input type="submit" name="save">
+    </form>
+</body>
+</html>
