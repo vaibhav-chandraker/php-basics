5a7ba136c88ca9054b9cfb47596b29975136d6a0
vaibhav-chandraker <Chandrakervaibhav2830@gmail.com>
2023-05-08 18:48:11 +0530

    Update 5_if_else.php

--- 5_if_else.php
+++ 5_if_else.php
@@ -12,4 +12,24 @@ else{
     echo "you cannot drive at all";
 }
 
+
+
+// switch statements
+
+$age = 45;
+
+switch ($age){
+    case 15:
+        echo "the age is 15";
+    case 25:
+        echo "the age is 25";
+    case 50:
+        echo "the age is 50";
+    default:
+        echo "your age is weird";
+
+
+}
+
+
 ?>
